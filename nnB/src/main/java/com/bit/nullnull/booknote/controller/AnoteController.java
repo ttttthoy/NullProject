ea94e631bd3aeee7b017fc6c5e9141652328224b
room403-02 <room403-02@DESKTOP-5BPQU8U>
2018-05-29 16:06:22 +0900

    팔로우 완료

--- nnB/src/main/java/com/bit/nullnull/booknote/controller/AnoteController.java
+++ nnB/src/main/java/com/bit/nullnull/booknote/controller/AnoteController.java
@@ -1,4 +1,4 @@
-package com.bit.nullnull.booknote.controller;
+	package com.bit.nullnull.booknote.controller;
 
 import javax.servlet.http.HttpSession;
 
@@ -11,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.bit.nullnull.booknote.model.Like;
 import com.bit.nullnull.booknote.model.Note;
 import com.bit.nullnull.booknote.service.noteLikeService;
+import com.bit.nullnull.member.model.Follow;
 import com.bit.nullnull.member.model.Member;
+import com.bit.nullnull.member.service.MemberFollowService;
+import com.bit.nullnull.member.service.MemberNumChkService;
 
 @Controller
 public class AnoteController {
@@ -19,10 +22,17 @@ public class AnoteController {
 	@Autowired
 	private noteLikeService nls;
 	
+	@Autowired
+	private MemberNumChkService mncs;
+	
+	@Autowired
+	private MemberFollowService mfs;
+			
 	@RequestMapping("/note/{note_id}/Anote")
 	public String noteMain(@PathVariable("note_id") int note_id, Model model, HttpSession session) {
 		
-		Boolean status = true;
+		Boolean l_status = true;
+		Boolean f_status = true;
 		
 		System.out.println("Note id : " + note_id);
 		
@@ -33,14 +43,25 @@ public class AnoteController {
 		Like like = new Like(note_id, member.getMember_num());
 		
 		if(nls.getLike(like) == null) {
-			status = false;
+			l_status = false;
+		}
+				
+		Member noteMem = mncs.selectById(note.getMember_num());
+		
+		Follow follow = new Follow(member.getMember_num(), noteMem.getMember_num());
+		
+		if(mfs.getFollow(follow) == null) {
+			f_status = false;
 		}
 		
-		model.addAttribute("status", status);
+		model.addAttribute("f_status", f_status);
+		model.addAttribute("l_status", l_status);
 		model.addAttribute("note", note);
+		model.addAttribute("noteMem", noteMem);
+		model.addAttribute("session", session.getAttribute("loginInfo"));
 			
 		return "note/Anote";
+		
 	}
-
 
 }
